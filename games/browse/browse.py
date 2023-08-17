4942e409305354bb2c0c26725a6be15d816e3088
Uni-tato <39069305+Uni-tato@users.noreply.github.com>
2023-08-17 17:54:41 +1200

    added non functional search and sort to browse

--- games/browse/browse.py
+++ games/browse/browse.py
@@ -8,7 +8,6 @@ browse_blueprint = Blueprint(
     'games_bp', __name__
 )
 
-
 @browse_blueprint.route('/browse', methods=['GET'])
 def browse_games():
     num_games = services.get_number_of_games(repo.repo_instance)
@@ -21,4 +20,19 @@ def browse_games():
         heading="Browse Games",
         games=all_games,
         num_games=num_games
+    )
+
+@browse_blueprint.route('/browse/<path:new_sort>', methods=['GET'])
+def browse_games_with_options(new_sort):
+    num_games = services.get_number_of_games(repo.repo_instance)
+    all_games = services.get_games(repo.repo_instance)
+    return render_template(
+        'browse.html',
+        # Custom page title
+        title=f"Browse Games | CS235 Game Library",
+        # Page heading
+        heading="Browse Games",
+        games=all_games,
+        num_games=num_games,
+        subpath=new_sort,
     )
