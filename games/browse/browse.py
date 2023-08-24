0fc79f2396b8334c136e595d5d3f6b36bedc2614
Uni-tato <39069305+Uni-tato@users.noreply.github.com>
2023-08-24 18:23:51 +1200

    adding tag nolonger clears search query

--- games/browse/browse.py
+++ games/browse/browse.py
@@ -14,23 +14,23 @@ def browse_games():
     print(f"url1 {list(request.args.items())}")
     search_string = request.args.get('search') if 'search' in request.args else None
     games = services.search_games(repo.repo_instance, title=search_string)
-    return _browse_games_render(games=games)
+    return _browse_games_render(games=games, cur_search=search_string)
 
 @browse_blueprint.route('/browse/', methods=['GET', 'POST'])
 def browse_games_with_slash():
     print(f"url2 {list(request.args.items())}")
     search_string = request.args.get('search') if 'search' in request.args else None
-    return redirect(url_for('games_bp.browse_games', search_string=search_string))
+    return redirect(url_for('games_bp.browse_games', search=search_string))
 
 @browse_blueprint.route('/browse/<path:subpath>', methods=['GET', 'POST'])
 def browse_games_with_options(subpath: str):
     print(f"url3 {list(request.args.items())}")
     subpath, tag_path, sort, tags, bad_url = services.parse_subpath(subpath, repo.repo_instance)
 
-    if bad_url: # Redirect to correct url
-        return redirect(url_for('games_bp.browse_games_with_options', subpath=subpath))
-    
     search_string = request.args.get('search') if 'search' in request.args else None
+
+    if bad_url: # Redirect to correct url
+        return redirect(url_for('games_bp.browse_games_with_options', subpath=subpath, search = search_string))
 
     games = services.search_games(repo.repo_instance, title=search_string, tags=tags)
     games = services.sort_games(games, sort)
@@ -40,6 +40,7 @@ def browse_games_with_options(subpath: str):
             cur_tags = tags,
             cur_subpath = subpath,
             cur_tag_path = tag_path,
+            cur_search = search_string,
             games = games)
 
 def _browse_games_render(
@@ -47,7 +48,7 @@ def _browse_games_render(
         cur_tags = [],
         cur_subpath = '',
         cur_tag_path = '',
-        num_games = 0,
+        cur_search = None,
         games = []):
     random_tags = repo.repo_instance.get_random_tags(5)
     return render_template(
@@ -60,6 +61,7 @@ def _browse_games_render(
         # Search Options
         cur_sort = cur_sort,
         cur_tags = cur_tags,
+        cur_search = cur_search,
         # Page URL
         cur_subpath = cur_subpath,
         cur_tag_path = cur_tag_path,
@@ -75,17 +77,27 @@ def _browse_games_render(
 
 @browse_blueprint.route('/browse/read_form', methods=['POST'])
 def browse_games_read_form():
+    
+    print("- args:")
+    for key, value in request.args.items():
+        print(f"key: {key}, value: {value}")
+    
+    print("- form:")
+    for key, value in request.form.items():
+        print(f"key: {key}, value: {value}")
+    
     # Read form data
     form_tags = []
     for key, value in request.form.items():
-        # print(f"key: {key}, value: {value}")
         if key == 'tag-dropdown' and value != 'null-selection':
             insort_left(form_tags, value)
         else:
             insort_left(form_tags, value)
 
-    subpath = request.args.get('subpath')
+    subpath = request.args.get('subpath') if 'subpath' in request.args else ''
     for tag in form_tags:
         subpath += '/' + tag
 
-    return redirect(url_for('games_bp.browse_games_with_options', subpath=subpath))
+    search_string = request.args.get('search') if 'search' in request.args else None
+
+    return redirect(url_for('games_bp.browse_games_with_options', subpath=subpath, search=search_string))
