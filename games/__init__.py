a4623946b419d621beba6dc505dee68c33566599
Stefan <stefan.zdra@gmail.com>
2023-08-28 13:45:40 +1200

    ⚰️ Unnecessary old stuff, begone!

--- games/__init__.py
+++ games/__init__.py
@@ -5,23 +5,6 @@ import games.adapters.repository as repo
 from games.adapters.memory_repository import populate
 from games.adapters.memory_repository import MemoryRepository
 
-# TODO: Access to the games should be implemented via the repository pattern and using blueprints, so this can not
-#  stay here!
-from games.domainmodel.model import Game
-
-
-# TODO: Access to the games should be implemented via the repository pattern and using blueprints, so this can not
-#  stay here!
-def create_some_game():
-    some_game = Game(1, "Call of Duty® 4: Modern Warfare®")
-    some_game.release_date = "Nov 12, 2007"
-    some_game.price = 9.99
-    some_game.description = "The new action-thriller from the award-winning team at Infinity Ward, the creators of " \
-                            "the Call of Duty® series, delivers the most intense and cinematic action experience ever. "
-    some_game.image_url = "https://cdn.akamai.steamstatic.com/steam/apps/7940/header.jpg?t=1646762118"
-    return some_game
-
-
 def create_app():
     """Construct the core application."""
 
@@ -43,9 +26,7 @@ def create_app():
 
     @app.route('/')
     def home():
-        some_game = create_some_game()
         # Use Jinja to customize a predefined html page rendering the layout for showing a single game.
         return render_template('main.html', heading="Discover new games")
-        # return render_template('gameDescription.html', game=some_game)
 
     return app
