fa0ee7e2cda8f40036a401f143993d3a02c5ba0e
Stefan <stefan.zdra@gmail.com>
2023-08-30 16:18:33 +1200

    🥅 404 page on invalid game id

--- tests/e2e/test_game_bp.py
+++ tests/e2e/test_game_bp.py
@@ -1,6 +1,7 @@
 import pytest
 
 def test_game_page(client):
+    # Test that the game page returns the correct games
     response = client.get('/game/457140')
     assert response.status_code == 200
     assert b'Oxygen Not Included' in response.data
@@ -10,3 +11,11 @@ def test_game_page(client):
     assert response.status_code == 200
     assert b'Super Meat Boy' in response.data
     assert b'114.99' in response.data
+
+def test_invaild_game_page(client):
+    # The web app should show a 404 page when the game doesn't exist/isn't found
+    response = client.get('/game/1')
+    assert response.status_code == 404
+
+    response = client.get('/game/-1')
+    assert response.status_code == 404
