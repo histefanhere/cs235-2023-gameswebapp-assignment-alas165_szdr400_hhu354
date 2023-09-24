0ee993aa74eda54a7b909c2b729e6ec48a60f03f
Uni-tato <39069305+Uni-tato@users.noreply.github.com>
2023-09-24 22:12:23 +1300

    Fixed profile authentication

--- games/profile/profile.py
+++ games/profile/profile.py
@@ -1,17 +1,13 @@
 from flask import Blueprint, render_template, session, redirect, url_for
 import games.profile.services as services
+from games.authentication.authentication import login_required
 
 profile_bp = Blueprint('profile_bp', __name__)
 
 @profile_bp.route('/profile')
+@login_required
 def profile():
+    print("hello")
     username = session.get('username')
-    if not username:
-        return redirect(url_for('auth_bp.login'))
-    
     user_data = services.get_user_data(username)
-    if not user_data:
-        # Handle the case where user data is not found
-        return "User data not found", 404
-    
     return render_template('profile.html', user=user_data)
