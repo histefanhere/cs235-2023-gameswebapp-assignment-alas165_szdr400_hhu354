e4ce854c0f40f4bdf498e29cc56a3fbfd57412a0
Uni-tato <39069305+Uni-tato@users.noreply.github.com>
2023-09-24 23:41:13 +1300

    Small preview of wishlist on profile

--- games/profile/services.py
+++ games/profile/services.py
@@ -1,9 +1,14 @@
-def get_user_data(username):
-    return {
-        'username': username,
-        'wishlist': [
-            {'title': 'Game Title 1', 'added_by': 'User1'},
-            {'title': 'Game Title 2', 'added_by': 'User2'},
-            {'title': 'Game Title 3', 'added_by': 'User3'}
-        ]
-    }
+from flask import session
+
+import games.adapters.repository as repo
+
+def get_short_wishlist():
+    user = repo.repo_instance.get_user(session['username'])
+    wishlist = user.wishlist
+    games = wishlist.list_of_games()
+    if len(games) > 3:
+        return games[:3]
+    else:
+        return games
+
+    
