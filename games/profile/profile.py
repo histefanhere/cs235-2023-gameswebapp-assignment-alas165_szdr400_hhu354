a44ac3dbfa04e4e6c076e68b8ab42d328dfd1e97
Sam Lui <hhu354@aucklanduni.ac.nz>
2023-09-24 21:36:35 +1300

    profile.py changes

--- games/profile/profile.py
+++ games/profile/profile.py
@@ -3,13 +3,15 @@ import games.profile.services as services
 
 profile_bp = Blueprint('profile_bp', __name__)
 
-
 @profile_bp.route('/profile')
 def profile():
-    if 'username' not in session:
+    username = session.get('username')
+    if not username:
         return redirect(url_for('auth_bp.login'))
     
-    username = session['username']
     user_data = services.get_user_data(username)
+    if not user_data:
+        # Handle the case where user data is not found
+        return "User data not found", 404
     
     return render_template('profile.html', user=user_data)
