949ee5140d0c26ca6c822ba3d3ddfde08903ae49
Sam Lui <hhu354@aucklanduni.ac.nz>
2023-09-24 23:36:59 +1300

    profile page update

--- games/profile/services.py
+++ games/profile/services.py
@@ -5,5 +5,10 @@ def get_user_data(username):
             {'title': 'Game Title 1', 'added_by': 'User1'},
             {'title': 'Game Title 2', 'added_by': 'User2'},
             {'title': 'Game Title 3', 'added_by': 'User3'}
+        ],
+        'game_reviews': [
+            {'title': 'Game Review 1', 'reviewer': 'User1', 'score': 8.5, 'date': '2023-09-01'},
+            {'title': 'Game Review 2', 'reviewer': 'User2', 'score': 9.0, 'date': '2023-09-05'},
+            {'title': 'Game Review 3', 'reviewer': 'User3', 'score': 7.8, 'date': '2023-09-10'}
         ]
     }
