930d10992b5725d9fe9f32029d65eb5959cf9392
Uni-tato <39069305+Uni-tato@users.noreply.github.com>
2023-09-25 00:04:02 +1300

    fix wishlist

--- games/profile/profile.py
+++ games/profile/profile.py
@@ -1,6 +1,7 @@
 from flask import Blueprint, render_template, session, redirect, url_for
 import games.profile.services as services
 from games.authentication.authentication import login_required
+import games.adapters.repository as repo
 
 profile_bp = Blueprint('profile_bp', __name__)
 
@@ -10,6 +11,7 @@ def profile():
     print(services.get_short_wishlist())
     return render_template(
         'profile.html',
+        user = repo.repo_instance.get_user(session['username']),
         short_wishlist=services.get_short_wishlist(),
         empty_wishlist=True if len(services.get_short_wishlist()) == 0 else False,
         )
