a6d1bbefe5de8dda1a15a069f181f0c1ecd93e72
Uni-tato <39069305+Uni-tato@users.noreply.github.com>
2023-09-24 22:17:28 +1300

    removed random test statement

--- games/profile/profile.py
+++ games/profile/profile.py
@@ -7,7 +7,6 @@ profile_bp = Blueprint('profile_bp', __name__)
 @profile_bp.route('/profile')
 @login_required
 def profile():
-    print("hello")
     username = session.get('username')
     user_data = services.get_user_data(username)
     return render_template('profile.html', user=user_data)
