a9d259de581ce8060ff9a8aba9945609791cb05d
Uni-tato <39069305+Uni-tato@users.noreply.github.com>
2023-10-14 20:02:01 +1300

    improved wishlist tests

--- tests/e2e/test_wishlist_bp.py
+++ tests/e2e/test_wishlist_bp.py
@@ -4,13 +4,40 @@ from flask import session
 
 from games.domainmodel.model import *
 
-def test_wishlist_page(client):
-    # Test that the wishlist page is not accessible to non-logged in users.
-    response = client.get("/wishlist")
-    assert response.status_code != 200
+def test_wishlist_page_unloggedin(client):
+    response = client.get('/wishlist')
+    assert response.headers['Location'] == '/authentication/login'
 
-    # This requires "an active HTTP request", we haven't been taught how to test this and there are more important things to worry about right now.
-    # session["username"] = "test"
-    # response = client.get("/wishlist")
-    # assert response.status_code == 200
+def test_adding_game_to_wishlist(repo, client, auth):
+    auth.login()
 
+    # Test wishlist starts empty
+    response = client.get('/wishlist')
+    assert b'Oxygen Not Included' not in response.data
+
+    # Test adding game.
+    client.post('/game/add_to_wishlist/457140')
+    response = client.get('/wishlist')
+    assert b'Oxygen Not Included' in response.data
+
+    
+
+def test_remove_from_wishlist_from_game_page(client, auth):
+    auth.login()
+
+    client.post('/game/add_to_wishlist/457140')
+
+    # Test remove from wishlist
+    client.post('/game/remove_from_wishlist/457140')
+    response = client.get('/wishlist')
+    assert b'Oxygen Not Included' not in response.data
+
+def test_remove_from_wishlist_from_wishlist_page(client, auth):
+    auth.login()
+
+    client.post('/game/add_to_wishlist/457140')
+
+    # Test remove from wishlist
+    client.post('/wishlist/remove/457140')
+    response = client.get('/wishlist')
+    assert b'Oxygen Not Included' not in response.data
