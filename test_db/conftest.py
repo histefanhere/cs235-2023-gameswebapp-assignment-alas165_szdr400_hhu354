cdc454ea1a6e79450a965199420818d00f8a0757
Uni-tato <39069305+Uni-tato@users.noreply.github.com>
2023-10-15 17:29:59 +1300

    correct repo object name

--- test_db/conftest.py
+++ test_db/conftest.py
@@ -17,12 +17,12 @@ def database_engine():
     clear_mappers()
     engine = create_engine(TEST_DATABASE_URI_FILE)
     metadata.create_all(engine)
-    for table in reversed(metadata.stored_tables):
+    for table in reversed(metadata.sorted_tables):
         engine.execute(table.delete())
     map_model_to_tables()
 
     session_factory = sessionmaker(autocommit=False, autoflush=True, bind=engine)
-    repo_instance = database_repository.SqlAlchemyRepository(session_factory)
+    repo_instance = database_repository.DatabaseRepository(session_factory)
     database_repository.populate(TEST_DATA_PATH, repo_instance)
     yield engine
     metadata.drop_all(engine)
@@ -32,12 +32,12 @@ def session_factory():
     clear_mappers()
     engine = create_engine(TEST_DATABASE_URI_IN_MEMORY)
     metadata.create_all(engine)
-    for table in reversed(metadata.stored_tables):
+    for table in reversed(metadata.sorted_tables):
         engine.execute(table.delete())
     map_model_to_tables()
 
     session_factory = sessionmaker(autocommit=False, autoflush=True, bind=engine)
-    repo_instance = database_repository.SqlAlchemyRepository(session_factory)
+    repo_instance = database_repository.DatabaseRepository(session_factory)
     database_repository.populate(TEST_DATA_PATH, repo_instance)
     yield session_factory
     metadata.drop_all(engine)
@@ -47,7 +47,7 @@ def empty_session():
     clear_mappers()
     engine = create_engine(TEST_DATABASE_URI_IN_MEMORY)
     metadata.create_all(engine)
-    for table in reversed(metadata.stored_tables):
+    for table in reversed(metadata.sorted_tables):
         engine.execute(table.delete())
     map_model_to_tables()
     
