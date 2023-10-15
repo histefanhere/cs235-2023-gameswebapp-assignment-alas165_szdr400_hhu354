3ef2648aa7a9e3f9552ff0c5709d9866dc42977f
Sam Lui <hhu354@aucklanduni.ac.nz>
2023-10-15 17:18:16 +1300

    :white_check_mark: Add unit test for user repository

---
+++ tests/unit/test_repository.py
@@ -0,0 +1,27 @@
+import pytest
+from games.adapters.repository import SqlAlchemyRepository
+from games.domainmodel.model import User
+
+def test_repository_can_add_a_user(session_factory):
+    repo = SqlAlchemyRepository(session_factory)
+
+    user = User('Dave', '123456789')
+    repo.add_user(user)
+
+    repo.add_user(User('Martin', '123456789'))
+
+    user2 = repo.get_user('Dave')
+
+    assert user2 == user and user2 is user
+
+def test_repository_can_retrieve_a_user(session_factory):
+    repo = SqlAlchemyRepository(session_factory)
+
+    user = repo.get_user('fmercury')
+    assert user == User('fmercury', '8734gfe2058v')
+
+def test_repository_does_not_retrieve_a_non_existent_user(session_factory):
+    repo = SqlAlchemyRepository(session_factory)
+
+    user = repo.get_user('prince')
+    assert user is None
